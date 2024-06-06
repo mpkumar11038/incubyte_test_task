ff563d9333634b11b4de77a5ef9aead9bb41c7a6
Mukesh Patel <mpkumar11038@gmail.com>
2024-06-06 15:08:29 +0530

    test_string_calculator_with_empty_input method  added to test empty string

--- custom_calculator.py
+++ custom_calculator.py
@@ -1,5 +1,6 @@
 def string_calculator(input_string):
+    if not input_string:
+        return 0
     numbers = input_string.split(',')
     total = sum([float(number) for number in numbers])
     return int(total)
-
--- test_calculator.py
+++ test_calculator.py
@@ -7,3 +7,7 @@ class TestStringCalculator(unittest.TestCase):
     def test_string_calculator(self):
         input_string = "1,2,3,4,5"
         self.assertEqual(string_calculator(input_string), 15)
+
+    def test_string_calculator_with_empty_input(self):
+        input_string = ""
+        self.assertEqual(string_calculator(input_string), 0)
